c1cb10a82192b211c9e0c7d9e44b3f06ef740844
mimi0379 <mimi0379@126.com>
2018-03-06 22:08:08 +0800

    添加ext classpath，boot classpath的展示

--- src/main/java/com/zqz/common/StartInit.java
+++ src/main/java/com/zqz/common/StartInit.java
@@ -2,6 +2,12 @@ package com.zqz.common;
 
 
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
@@ -16,6 +22,14 @@ import com.zqz.jvm.task.job.ConnectTask;
  */
 public class StartInit implements ApplicationListener<ContextRefreshedEvent> {
 	private static Logger logger = LoggerFactory.getLogger(ConnectTask.class);
+	static{
+		RuntimeMXBean osBean = ManagementFactory.getPlatformMXBean(RuntimeMXBean.class);
+		Map<String,String> map = osBean.getSystemProperties();
+		Set<Entry<String,String>> set = map.entrySet();
+		for(Entry<String,String> e: set){
+			System.out.println(e.getKey()+"=="+e.getValue());
+		}
+	}
 
 	/**
 	 * 启动时初始化放在这里执行
